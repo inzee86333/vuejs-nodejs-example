81cfa5cb18782218d0832e0cf6774cff557822de
Nitipat L <iamsvz@gmail.com>
2021-01-14 19:12:32 +0700

    [Edited] fixed some bug

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const randomId = require('random-id');
 const app = express(),
       bodyParser = require("body-parser");
-      port = 3080;
+      port = process.env.PORT || 3080;
 
 // place holder for the data
 const users = [];
